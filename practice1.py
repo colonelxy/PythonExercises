1eb97b77956de2eedd351e31dd7bf29a4efee140
colonelxy <harold2@protonmail.com>
2022-06-12 09:43:58 +0300

    Looping and summing

--- practice1.py
+++ practice1.py
@@ -7,12 +7,37 @@ from logging import logProcesses
 for i in [5,4,3,2,1]:
     print(i)
 print("Get out of the house!")
-print("")
+print("") #Create a space before the next function
+
 #Here, i is a moving position indicator that runs through the values from left to right
 
 #Using stored variables
-#We are using friend instead of i, but you can use anyword or letter to represen the looping tool
+#We are using friend instead of i, but you can use anyword or letter to represent the looping tool
 friends = ['John', 'Jerry', 'Jammie']
 for friend in friends:
     print('Happy Birthday:', friend)
 print('Done!')
+print('')
+
+#What's the largest number?
+#How do you identify the largest number efficiently?
+#Start with a variable with a -1
+largestSoFar = -1
+print( 'Start', largestSoFar)
+for number in [3,41,12,9,74,15] : # loop through each number and keep changing the value of largestSoFar
+    if number > largestSoFar :
+        largestSoFar = number
+    print(largestSoFar, number)
+print('End', largestSoFar)
+print('')
+
+#Summing in a loop
+Rocks =0
+print('Before', Rocks)
+for i in [3,41,12,9,74,15] :
+    Rocks = Rocks + i #Adding the item value to the initial Rocks value
+    print(Rocks ,i)
+print("After", Rocks) #The running total
+
+#Find the average
+
