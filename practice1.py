a2fc97fe35330dfc34127e2346d952691a42bb38
colonelxy <harold2@protonmail.com>
2022-06-12 09:25:40 +0300

    Practice 1

---
+++ practice1.py
@@ -0,0 +1,18 @@
+from logging import logProcesses
+
+
+#Definite Loop
+
+#Looping through an array
+for i in [5,4,3,2,1]:
+    print(i)
+print("Get out of the house!")
+print("")
+#Here, i is a moving position indicator that runs through the values from left to right
+
+#Using stored variables
+#We are using friend instead of i, but you can use anyword or letter to represen the looping tool
+friends = ['John', 'Jerry', 'Jammie']
+for friend in friends:
+    print('Happy Birthday:', friend)
+print('Done!')
