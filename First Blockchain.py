668f3ff5cc9d87de831eb8cd605913c41b3f6a78
colonelxy <harold2@protonmail.com>
2022-06-15 19:09:38 +0300

    Validating block

--- First Blockchain.py
+++ First Blockchain.py
@@ -5,7 +5,7 @@ import datetime
 import hashlib
 
 # For storing data in the blockchain
-import _json
+import json
 
 # We need to create the web app hence this flask. jsonify displays the blockchain
 from flask import Flask, jsonify
@@ -37,6 +37,80 @@ class Blockchain:
 
         while check_proof is False :
             hash_operation = hashlib.sha256(
-                str(new_proof**2 - previous_proof**2).encode()
-            )
-            
+                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:5] == '00000' :
+                check_proof = True 
+            else :
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block) :
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def chain_valid(self, chain) :
+        previous_block =  chain[0]
+        block_index = 1
+
+        while block_index < len(chain) :
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block) :
+                return False 
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
+
+            if hash_operation[:5] != '00000' :
+                return False 
+            previous_block = block
+            block_index +=1
+        return True 
+# Creating the web app using flask 
+app = Flask(__name__)
+
+
+#  Create the object of the class Blockchain
+blockchain = Blockchain()
+
+# Mining a new block 
+@app.route('/mine_block', methods = ['GET'])
+def mine_block() :
+    previous_block = blockchain.print_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {'message' : 'A block is MINED', 'index' : block['index'], 'timestamp' : block['timestamp'], 'proof' : block['proof'], 'previous_hash' : block['previous_hash']}
+
+    return jsonify(response), 200
+
+# Display blockchain in json format 
+@app.route('/get_chain', methods=['GET'])
+def display_chain() :
+    response = {'chain' : blockchain.chain,     'length' : len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Check validity of the blockchain 
+@app.route('/valid', methods=['GET'])
+def valid() :
+    valid = blockchain.chain_valid(blockchain.chain)
+    if valid:
+        response = {'message' : 'The blockchain is valid.'}
+    else:
+            response = {'message': 'The blockchaini s not valid.'}
+            return jsonify(response), 200
+
+
+
+# Run the flask locally
+app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+    
+    
