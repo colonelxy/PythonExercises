34cc6744c04ce7ada9f830222de41939eeaefb30
colonelxy <harold2@protonmail.com>
2022-06-15 17:20:29 +0300

    Import datetime, hashlib, json, flask, jsonify

---
+++ First Blockchain.py
@@ -0,0 +1,14 @@
+# For the time stamp function
+import datetime
+
+# For calculating the hash to add thr digital fingerprints to the blocks
+import hashlib
+
+# For storing data in the blockchain
+import _json
+
+# We need to create the web app hence this flask. jsonify displays the blockchain
+from flask import Flask, jsonify
+
+# Let's begin
+
