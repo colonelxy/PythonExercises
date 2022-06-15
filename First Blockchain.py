cafffaf7c56efd68bee156bffe618e659db62a0e
colonelxy <harold2@protonmail.com>
2022-06-15 18:08:47 +0300

    create timestamp, show previous block

--- First Blockchain.py
+++ First Blockchain.py
@@ -17,4 +17,14 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
-        
+    # Add further blocks to the chain 
+    def create_block(self, proof, previous_hash) :
+        block = {'index': len(self.chain) + 1,
+                'timestamp' : str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash' : previous_hash}
+        self.chain.append(block)
+        return block
+
+    # We want now to display the previous block
+    
